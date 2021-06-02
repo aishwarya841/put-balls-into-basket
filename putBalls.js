7b5b1c94a14e4e82b3b7c534dc0a1a90e51250c5
Aishwarya Dubey <aishwaryadubey1234@gmail.com>
2021-06-02 21:11:59 +0530

    Modified Color

--- putBalls.js
+++ putBalls.js
@@ -1,7 +1,7 @@
 
-var colors = ['red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black', 'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen', 'deepskyblue', 'firebrick','crimson']
+var colors = ['red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black', 'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen', 'deepskyblue', 'firebrick','crimson','black']
 $('button').click(function(){
-    var count = Math.floor(Math.random() * (16 - 0)) + 0;
+    var count = Math.floor(Math.random() * (17 - 0)) + 0;
     var ball = $('.ball')[0];
     $('#basket').prepend('<div class="ball" style="background-color:'+colors[count]+';"></div>');
     
